d479742e999f97e9a7eb9ea5f954ace1d4a459b1
Gooya <joseph.shingoo@gmail.com>
2020-08-10 00:46:04 +0900

    snack bar

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: '꼬투리 감사',
+      title: 'Snack Bar',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        backgroundColor: Colors.white,
       ),
       home: MyPage(),
     );
@@ -21,93 +21,34 @@ class MyPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('꼬투리 감사'),
-        backgroundColor: Colors.amber[800],
+        title: Text('Snack Bar'),
+        backgroundColor: Colors.red[600],
         centerTitle: true,
-        elevation: 0.0,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {
-              print('shopping_cart button is clicked!');
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.search),
-            onPressed: () {
-              print('search button is clicked!');
-            },
-          ),
-        ],
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/logo.png'),
-                backgroundColor: Colors.white,
-              ),
-              accountName: Text('SHINGOO'),
-              accountEmail: Text('joseph.shingoo@gmail.com'),
-              onDetailsPressed: () {
-                print('detail button is clicked!');
-              },
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(40.0),
-                  bottomRight: Radius.circular(40.0),
-                ),
+      body: MySnackBar(),
+    );
+  }
+}
+
+class MySnackBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RaisedButton(
+        child: Text('Show me'),
+        onPressed: () {
+          Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text(
+              'Hello',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
               ),
             ),
-            ListTile(
-              leading: Icon(
-                Icons.home,
-                color: Colors.grey[850],
-              ),
-              title: Text('home'),
-              onTap: () {
-                print('home is clicked!');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.settings,
-                color: Colors.grey[850],
-              ),
-              title: Text('Settings'),
-              onTap: () {
-                print('settings is clicked!');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.question_answer,
-                color: Colors.grey[850],
-              ),
-              title: Text('QNA'),
-              onTap: () {
-                print('QNA is clicked!');
-              },
-              trailing: Icon(Icons.add),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.time_to_leave,
-                color: Colors.grey[850],
-              ),
-              title: Text('TTL'),
-              onTap: () {
-                print('TTL is clicked!');
-              },
-              trailing: Icon(Icons.add),
-            ),
-          ],
-        ),
+            backgroundColor: Colors.teal,
+            duration: Duration(milliseconds: 1000),
+          ));
+        },
       ),
     );
   }
